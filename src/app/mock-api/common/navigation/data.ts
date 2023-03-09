eb4112a8335602a8167bf2b67fcbdd4b711cbd72
grootersluiz <luizjr.800@gmail.com>
2023-03-09 19:07:49 -0300

    Tabela Padrão 1

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,67 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id: 'admin.ecommerce',
+        title: 'ECommerce',
+        type: 'collapsable',
+        icon: 'heroicons_outline:shopping-cart',
+        children: [
+            {
+                id: 'admin.ecommerce.inventory',
+                title: 'Inventory',
+                type: 'basic',
+                link: '/admin/ecommerce/inventory',
+            },
+        ],
+    },
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id: 'admin.ecommerce',
+        title: 'ECommerce',
+        type: 'collapsable',
+        icon: 'heroicons_outline:shopping-cart',
+        children: [
+            {
+                id: 'admin.ecommerce.inventory',
+                title: 'Inventory',
+                type: 'basic',
+                link: '/admin/ecommerce/inventory',
+            },
+        ],
+    },
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id: 'admin.ecommerce',
+        title: 'ECommerce',
+        type: 'collapsable',
+        icon: 'heroicons_outline:shopping-cart',
+        children: [
+            {
+                id: 'admin.ecommerce.inventory',
+                title: 'Inventory',
+                type: 'basic',
+                link: '/admin/ecommerce/inventory',
+            },
+        ],
+    },
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id: 'admin.ecommerce',
+        title: 'ECommerce',
+        type: 'collapsable',
+        icon: 'heroicons_outline:shopping-cart',
+        children: [
+            {
+                id: 'admin.ecommerce.inventory',
+                title: 'Inventory',
+                type: 'basic',
+                link: '/admin/ecommerce/inventory',
+            },
+        ],
+    },
+];
