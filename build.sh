f9d59d6af92115ed48e31536c5bc37d50d8ba73a
ralfam.portela <ralfam.portela@3db.cloud>
2023-04-04 15:41:50 -0300

    Configuração Yaml e build para o front

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#=================================================================#     
+#Arquivo: build.sh                                                #  
+#Autor: Ralfam Portela         Email: ralfam.portela@3db.cloud    #      
+#Contribuição Sebastião Santos Email: sebastiao.santos@3db.cloud  #      
+#Data: 21-10-2022                                                 # 
+#                                                                 # 
+#Esse arquivo é de build de imagens jsp-bi  Front                 #  
+#                                                                 #
+#                                                                 #
+#                                                                 #  
+#=================================================================#     
+
+#Colocar o dados da cloud
+USER="grj7uyekqpbi/cloud-adm-jsp@3db.net.br"
+
+TENANT="grj7uyekqpbi" 
+
+APP="jsp-bi-front"
+VERSION="1.0.2"
+
+echo "Construindo a imagem ${VERSION}"
+#Faz um build baseado no Dockfile
+docker build -t ${VERSION} .
+
+echo "Marcando a tag latest também"
+#Faz duas tags sendo a latest será a que usaremos em produção
+docker tag ${VERSION} gru.ocir.io/${TENANT}/${APP}:${VERSION}
+docker tag ${VERSION} gru.ocir.io/${TENANT}/${APP}:latest
+
+#Envia as duas imagens para o OCI Container Registry
+echo "Enviando a imagem para nuvem docker"
+docker push gru.ocir.io/${TENANT}/${APP}:${VERSION}
+docker push gru.ocir.io/${TENANT}/${APP}:latest
