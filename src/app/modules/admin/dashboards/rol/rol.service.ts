d555dd3d39c49fbdf288f341511e8bde245c28a4
grootersluiz <luizjr.800@gmail.com>
2023-04-12 15:15:57 -0300

    Atualização Endpoint Dash ROL

--- src/app/modules/admin/dashboards/rol/rol.service.ts
+++ src/app/modules/admin/dashboards/rol/rol.service.ts
@@ -33,23 +33,22 @@ export class RolService {
      * Get data
      */
     getData(): Observable<any> {
-        return this._httpClient.get('http://10.2.1.108/v1/reports/3/data').pipe(
-            tap((response: any) => {
-                const chartData = {
-                    ...response[4],
-                    series: {
-                        'this-year': response[4].series.map((item) => ({
-                            ...item,
-                            data: item.values,
-                        })),
-                    },
-                };
+        return this._httpClient
+            .get('http://10.2.1.108/v1/dashboards/data?reportId=3&reportId=21')
+            .pipe(
+                tap((response: any) => {
+                    const chartData = {
+                        ...response['3']['2022'],
+                        series: {
+                            'this-year': response['3']['2022'].series,
+                        },
+                    };
 
-                console.log('rol', chartData);
-                console.log('githubIssues', rol.githubIssues);
-                const dashData = { ...rol, githubIssues: chartData };
-                this._data.next(dashData);
-            })
-        );
+                    console.log('rol', chartData);
+                    console.log('githubIssues', rol.githubIssues);
+                    const dashData = { ...rol, githubIssues: chartData };
+                    this._data.next(dashData);
+                })
+            );
     }
 }
