5cfcab421196d9b7d4a5e3ef697bde5cc437eed5
Ludemeury Fernandes de Sá <ludemeury@gmail.com>
2023-04-17 19:27:40 -0300

    build 1.0.0

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ USER="grj7uyekqpbi/ludemeury@vedastech.com.br"
 TENANT="grj7uyekqpbi" 
 
 APP="jsp-bi-front"
-VERSION="1.0.4"
+VERSION="1.0.5"
 
 echo "Construindo a imagem ${VERSION}"
 #Faz um build baseado no Dockfile
