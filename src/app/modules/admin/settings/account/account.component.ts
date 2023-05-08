e588753ca890dc58a5ad32d30eff93425f7848dc
grootersluiz <luizjr.800@gmail.com>
2023-05-08 17:24:36 -0300

    Alteração Tela Configurações Info Read Only API

--- src/app/modules/admin/settings/account/account.component.ts
+++ src/app/modules/admin/settings/account/account.component.ts
@@ -9,6 +9,9 @@ import {
     UntypedFormGroup,
     Validators,
 } from '@angular/forms';
+import { User } from 'app/core/user/user.types';
+import { UserService } from 'app/core/user/user.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'settings-account',
@@ -19,11 +22,16 @@ import {
 export class SettingsAccountComponent implements OnInit {
     accountForm: UntypedFormGroup;
     securityForm: UntypedFormGroup;
+    user: User;
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
 
     /**
      * Constructor
      */
-    constructor(private _formBuilder: UntypedFormBuilder) {}
+    constructor(
+        private _formBuilder: UntypedFormBuilder,
+        private _userService: UserService
+    ) {}
 
     // -----------------------------------------------------------------------------------------------------
     // @ Lifecycle hooks
@@ -33,26 +41,25 @@ export class SettingsAccountComponent implements OnInit {
      * On init
      */
     ngOnInit(): void {
+        this._userService.user$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((user: User) => {
+                this.user = user;
+            });
+
         // Create the form
         this.accountForm = this._formBuilder.group({
-            name: ['Luiz Grooters Jr.'],
-            username: ['luizg'],
-            title: ['Dev Front-End'],
-            company: ['Vedas Tech'],
-            about: [
-                "Hey! This is Brian; husband, father and gamer. I'm mostly passionate about bleeding edge tech and chocolate! 🍫",
-            ],
-            email: ['grooters.luiz@jspecas.com.br', Validators.email],
-            phone: ['62-98175-0604'],
-            country: ['usa'],
-            language: ['english'],
+            name: [this.user.name],
+            email: [this.user.email],
+            role: [this.user.role],
+            id: [this.user.id],
         });
         // Create the form
-        this.securityForm = this._formBuilder.group({
-            currentPassword: [''],
-            newPassword: [''],
-            twoStep: [true],
-            askPasswordChange: [false],
-        });
+        // this.securityForm = this._formBuilder.group({
+        //     currentPassword: [''],
+        //     newPassword: [''],
+        //     twoStep: [true],
+        //     askPasswordChange: [false],
+        // });
     }
 }
