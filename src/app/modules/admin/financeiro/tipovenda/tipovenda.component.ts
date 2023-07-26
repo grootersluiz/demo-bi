145b938be8d0c8d0986b1468c705c288d9325a04
AlexLCarv <alexgoiasgol10@hotmail.com>
2023-07-26 13:58:26 -0300

    Adicionado graficos no TIpo de Vendas para demonstração

--- src/app/modules/admin/financeiro/tipovenda/tipovenda.component.ts
+++ src/app/modules/admin/financeiro/tipovenda/tipovenda.component.ts
@@ -25,6 +25,17 @@ import {
     chart: ApexChart;
     responsive: ApexResponsive[];
     labels: any;
+
+  };
+  export type ChartOptions1 = {
+    series: ApexAxisChartSeries;
+    chart: ApexChart;
+    dataLabels: ApexDataLabels;
+    plotOptions: ApexPlotOptions;
+    responsive: ApexResponsive[];
+    xaxis: ApexXAxis;
+    legend: ApexLegend;
+    fill: ApexFill;
   };
 
 @Component({
@@ -38,6 +49,7 @@ import {
 export class tipovendaComponent {
     @ViewChild("chart") chart: ChartComponent;
     public chartOptions: Partial<ChartOptions>;
+    public chartOptions1: Partial<ChartOptions1>;
     titulo: string = "Financeiro";
     subTitulo: string = '';
     isToggleOn: boolean;
@@ -64,5 +76,76 @@ export class tipovendaComponent {
             }
           ]
         };
+        this.chartOptions1 = {
+            series: [
+              {
+                name: "PRODUCT A",
+                data: [44, 55, 41, 67, 22, 43, 21, 17, 25, 13, 22, 18]
+              },
+              {
+                name: "PRODUCT B",
+                data: [13, 23, 20, 8, 13, 27, 21, 17, 25, 13, 22, 18]
+              },
+              {
+                name: "PRODUCT C",
+                data: [11, 17, 15, 15, 21, 14, 21, 17, 25, 13, 22, 18]
+              },
+              {
+                name: "PRODUCT D",
+                data: [21, 17, 25, 13, 22, 18, 18, 20,10,12,12,15]
+              }
+            ],
+            chart: {
+              type: "bar",
+              height: 220,
+              stacked: true,
+              toolbar: {
+                show: true
+              },
+              zoom: {
+                enabled: true
+              }
+            },
+            responsive: [
+              {
+                breakpoint: 480,
+                options: {
+                  legend: {
+                    position: "bottom",
+                    offsetX: -10,
+                    offsetY: 0
+                  }
+                }
+              }
+            ],
+            plotOptions: {
+              bar: {
+                horizontal: false
+              }
+            },
+            xaxis: {
+              type: "category",
+              categories: [
+                "01/2011",
+                "02/2011",
+                "03/2011",
+                "04/2011",
+                "05/2011",
+                "06/2011",
+                "07/2011",
+                "08/2011",
+                "09/2011",
+                "10/2011",
+                "11/2011",
+                "12/2011"
+              ]
+            },
+            legend: {
+              position: "bottom"
+            },
+            fill: {
+              opacity: 1
+            }
+          };
       }
 }
