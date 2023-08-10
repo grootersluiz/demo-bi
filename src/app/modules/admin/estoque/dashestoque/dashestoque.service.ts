d9d3c481ea7f3a4c1ae4daf004f57ada6bc02081
Wagdson Ferreira Maia <wagdson.ope@jspecas.com.br>
2023-08-10 18:00:13 -0300

    Adiciando o Giro de Estoque, e disponibilidade de Curva e Agrupador

--- src/app/modules/admin/estoque/dashestoque/dashestoque.service.ts
+++ src/app/modules/admin/estoque/dashestoque/dashestoque.service.ts
@@ -6,9 +6,35 @@ import { Observable } from 'rxjs';
     providedIn: 'root'
 })
 export class DashEstoqueService {
+   
 
+    constructor(private _httpClient: HttpClient) {
 
-  
     }
+
+
+
+    getDiasEstoque(codemp: String[]): Observable<any> {
+     
+        return this._httpClient.get<{ columns: []; rows: [] }>(
+            'http://api.portal.jspecas.com.br/v1/views/519/data?' +
+            'codemp=' +
+            codemp
+        );
+    }
+
+    getGiroEstoque(codemp: String[]): Observable<any> {
+
+
+        return this._httpClient.get<{ columns: []; rows: [] }>(
+            'http://api.portal.jspecas.com.br/v1/views/524/data?' +
+            'codemp=' +
+            codemp
+        );
+      
+    }
+
+
+}
 
 
